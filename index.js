933c397b220672bd662c08fc9c7870d634ab4f04
MrJoCrafter <cstudios35511@gmail.com>
2018-03-10 15:00:47 -0600

    Add 1, Remove 2 commands. Also fix some behind-the-scenes stuff.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
+var history = [];
 bot.on('message', (message) => {
-
-  if(message.content == '!ping'){
-    message.reply('Pong!');
+  console.log('message detected');
+  if(message.author.username != "Shadow Bot" && message.content != "!quote" && message.content != "!help"){
+    history.push(message.author.username + ': ' + message.content);
+  }
+  // console.log(history);
+  if(message.content == '!quote'){
+    var messages = history.length;
+    var random = Math.floor(Math.random() * messages);
+    message.channel.sendMessage(history[random]);
+  }
+  if(message.content == '!help'){
+    message.reply("\nCommands:\n!help: Show all commands.\n!quote: Quote a random message from this channel!");
   }
 })
-bot.login('NDIxODM4OTYyMjM2MDYzNzQ1.DYTDvw.iFp1ECnIX88y3XANVq3hRKD6WjE');
+bot.login('token hidden. please use the link in readme.');
