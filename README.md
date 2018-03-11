afa2f3f9222fd976ea9b5d718c3ade4e87dea08e
MrJoCrafter <cstudios35511@gmail.com>
2018-03-10 21:07:23 -0600

    prefix customizability, kinda?

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Commands:
 !help = show commands.
 !coinflip = flip a coin you bean.
-
+!roll = roll a dice.
+!quote = pull a message from the bot's memory.
 
 Want to run your own instance of this bot? I mean you can, but please don't, just use this link instead:
 https://discordapp.com/oauth2/authorize?client_id=421838962236063745&scope=bot&permissions=2146958591
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 var history = [];
+var prefix = "!";
 bot.on('message', (message) => {
+  if(prefix == undefined){
+    prefix = '!';
+  }
   console.log('message detected');
-  if(message.author.username != "Shadow Bot" && message.content != "!quote" && message.content != "!help" &&
-  message.content != "!roll" && message.content != "!coinflip"){
+  if(message.author.username != "Shadow Bot" && message.content != prefix + "quote" &&
+  message.content != prefix + "help" && message.content != "%&!prefix" &&
+  message.content != prefix + "roll" && message.content != prefix + "coinflip"){
     history.push(message.author.username + ': ' + message.content);
   }
   // console.log(history);
-  if(message.content == '!coinflip'){
+  if(message.content == prefix + 'coinflip'){
     var random = Math.floor(Math.random() * 2) + 1;
     if(random == 1){
       random = 'heads';
@@ -17,22 +22,56 @@ bot.on('message', (message) => {
     }
     message.reply('Your coin landed on ' + random);
   }
-  if(message.content == '!roll'){
+  //Roll a dice.
+  if(message.content == prefix + 'roll'){
     var random = Math.floor(Math.random() * 6) + 1;
     message.reply('You rolled a ' + random);
   }
-  if(message.content == '!quote'){
+  //Change the prefix
+  if(message.content == '%&!prefix %' && message.author.username == "MrJoCrafter"){
+    message.channel.send("I've set the command prefix to % for you!");
+  }else if(message.content == '%&!prefix %' && message.author.username == "MrJoCrafter"){
+    prefix = "%";
+    message.channel.send("I've set the prefix to " + prefix);
+  }else if(message.content == '%&!prefix @' && message.author.username == "MrJoCrafter"){
+    prefix = "@";
+    message.channel.send("I've set the prefix to " + prefix);
+  }else if(message.content == '%&!prefix #' && message.author.username == "MrJoCrafter"){
+    prefix = "#";
+    message.channel.send("I've set the prefix to " + prefix);
+  }else if(message.content == '%&!prefix $' && message.author.username == "MrJoCrafter"){
+    prefix = "$";
+    message.channel.send("I've set the prefix to " + prefix);
+  }else if(message.content == '%&!prefix |' && message.author.username == "MrJoCrafter"){
+    prefix = "|";
+    message.channel.send("I've set the prefix to " + prefix);
+  }else if(message.content == '%&!prefix !' && message.author.username == "MrJoCrafter"){
+    prefix = "!";
+    message.channel.send("I've set the prefix to " + prefix);
+  }else if(message.content == '%&!prefix' && message.author.username == "MrJoCrafter"){
+    message.channel.send("Not a valid prefix. Please use @,#,$,%,|, or !");
+  }
+  //Reset the prefix to !
+  if(message.content == '%&!reset' && message.author.username == "MrJoCrafter"){
+    prefix = '!';
+    message.channel.send("I've reset the command prefix to ! for you!");
+  }
+  //Random quote from bot's memory.
+  if(message.content == prefix + 'quote'){
     var messages = history.length;
     var random = Math.floor(Math.random() * messages);
-    message.channel.sendMessage(history[random]);
+    message.channel.send(history[random]);
   }
-  if(message.content == '!help'){
-    message.reply("\nCommands:" +
-    "\n!help: Show all commands." +
-    "\n!quote: Quote a random message from this channel!" +
-    "\n!roll: Roll a dice." +
-    "\n!coinflip: Flip a coin."
+  //Show all commands
+  if(message.content == prefix + 'help'){
+    message.reply("\n" + "Commands:" +
+    "\n" + prefix + "help: Show all commands." +
+    "\n" + prefix + "quote: Quote a random message from this channel!" +
+    "\n" + prefix + "roll: Roll a dice." +
+    "\n" + prefix + "coinflip: Flip a coin." +
+    "\n" + "%&!prefix: Change the command prefix. (Owner Only)" +
+    "\n" + "%&!reset: Reset the command prefix to ! . (Owner Only)"
     );
   }
 })
-bot.login('token removed for security reasons.');
+bot.login('NDIxODM4OTYyMjM2MDYzNzQ1.DYYBQA.TFgYmHBwwpBDaQeRt8CFfbB5Bow');
