d34620e50b20a35ac29d1d84bbaf3aaf9a88a028
MrJoCrafter <cstudios35511@gmail.com>
2018-03-11 11:35:08 -0500

    4 new commands, and add full customizability to prefix

--- README.md
+++ README.md
@@ -9,7 +9,13 @@
 
 !quote = pull a message from the bot's memory.
 
-### Want to run your own instance of this bot? 
+!define = define a word using merriam-webster.
+
+!repo = link to the repository.
+
+!join = send join link.
+
+### Want to run your own instance of this bot?
 I mean you can, but please don't, just use this link instead:
 
 https://discordapp.com/oauth2/authorize?client_id=421838962236063745&scope=bot&permissions=2146958591
--- index.js
+++ index.js
@@ -7,12 +7,38 @@ bot.on('message', (message) => {
     prefix = '!';
   }
   console.log('message detected');
-  if(message.author.username != "Shadow Bot" && message.content != prefix + "quote" &&
-  message.content != prefix + "help" && message.content != "%&!prefix" &&
-  message.content != prefix + "roll" && message.content != prefix + "coinflip"){
+  if(message.author.username != "DuckHunt" && message.author.username != "Shadow Bot" && message.content != prefix + "quote" &&
+  message.content != prefix + "help" && !message.content.startsWith("%&!prefix ") && message.content != "%&!reset" &&
+  message.content != "%&!resetprefix" && message.content != prefix + "roll" && message.content != prefix + "coinflip" &&
+  !message.content.startsWith(prefix + "define") && message.content != prefix + "history" && message.content != prefix + "repo" &&
+  message.content != prefix + "repository" && message.content != prefix + "join" && message.content != prefix + "joinserver"){
     history.push(message.author.username + ': ' + message.content);
   }
-  // console.log(history);
+
+  //send history to owner
+  if(message.content == prefix + 'history' && message.author.username == "MrJoCrafter"){
+    message.channel.send("History retrieved!");
+    message.author.send(history);
+  }
+
+  //join a server
+  if(message.content == prefix + 'join' || message.content == prefix + 'joinserver'){
+    message.channel.send("Use this link: https://discordapp.com/oauth2/authorize?client_id=421838962236063745&scope=bot&permissions=2146958591");
+  }
+
+  //link the repo
+  if(message.content == prefix + 'repo' || message.content == prefix + 'repository'){
+    message.channel.send("https://github.com/TeamCstudios/ShadowBot");
+  }
+
+  //Define a word
+  if (message.content.startsWith(prefix + "define ")){
+        var commandline = prefix + "define ";
+        var argument = message.content.substr(commandline.length);
+        message.channel.send("https://www.merriam-webster.com/dictionary/" + argument);
+  }
+
+  //Flip a coin.
   if(message.content == prefix + 'coinflip'){
     var random = Math.floor(Math.random() * 2) + 1;
     if(random == 1){
@@ -22,46 +48,34 @@ bot.on('message', (message) => {
     }
     message.reply('Your coin landed on ' + random);
   }
+
   //Roll a dice.
   if(message.content == prefix + 'roll'){
     var random = Math.floor(Math.random() * 6) + 1;
     message.reply('You rolled a ' + random);
   }
+
   //Change the prefix
-  if(message.content == '%&!prefix %' && message.author.username == "MrJoCrafter"){
-    message.channel.send("I've set the command prefix to % for you!");
-  }else if(message.content == '%&!prefix %' && message.author.username == "MrJoCrafter"){
-    prefix = "%";
-    message.channel.send("I've set the prefix to " + prefix);
-  }else if(message.content == '%&!prefix @' && message.author.username == "MrJoCrafter"){
-    prefix = "@";
-    message.channel.send("I've set the prefix to " + prefix);
-  }else if(message.content == '%&!prefix #' && message.author.username == "MrJoCrafter"){
-    prefix = "#";
-    message.channel.send("I've set the prefix to " + prefix);
-  }else if(message.content == '%&!prefix $' && message.author.username == "MrJoCrafter"){
-    prefix = "$";
-    message.channel.send("I've set the prefix to " + prefix);
-  }else if(message.content == '%&!prefix |' && message.author.username == "MrJoCrafter"){
-    prefix = "|";
-    message.channel.send("I've set the prefix to " + prefix);
-  }else if(message.content == '%&!prefix !' && message.author.username == "MrJoCrafter"){
-    prefix = "!";
-    message.channel.send("I've set the prefix to " + prefix);
-  }else if(message.content == '%&!prefix' && message.author.username == "MrJoCrafter"){
-    message.channel.send("Not a valid prefix. Please use @,#,$,%,|, or !");
+  if (message.content.startsWith("%&!prefix ")){
+        var commandline = "%&!prefix ";
+        var argument = message.content.substr(commandline.length);
+        prefix = argument;
+        message.channel.send("I've set the prefix to " + prefix + " If you messed up, do %&!reset");
   }
+
   //Reset the prefix to !
-  if(message.content == '%&!reset' && message.author.username == "MrJoCrafter"){
+  if(message.content == '%&!reset' || message.content == '%&!resetprefix' && message.author.username == "MrJoCrafter"){
     prefix = '!';
     message.channel.send("I've reset the command prefix to ! for you!");
   }
+
   //Random quote from bot's memory.
   if(message.content == prefix + 'quote'){
     var messages = history.length;
     var random = Math.floor(Math.random() * messages);
     message.channel.send(history[random]);
   }
+
   //Show all commands
   if(message.content == prefix + 'help'){
     message.reply("\n" + "Commands:" +
@@ -69,9 +83,13 @@ bot.on('message', (message) => {
     "\n" + prefix + "quote: Quote a random message from this channel!" +
     "\n" + prefix + "roll: Roll a dice." +
     "\n" + prefix + "coinflip: Flip a coin." +
-    "\n" + "%&!prefix: Change the command prefix. (Owner Only)" +
+    "\n" + prefix + "define [word]: Define a word." +
+    "\n" + prefix + "repo: Link the repo." +
+    "\n" + prefix + "join: Send server join link." +
+    "\n" + prefix + "history: Send memory to owner (Owner Only)" +
+    "\n" + "%&!prefix [prefix]: Change the command prefix. (Owner Only)" +
     "\n" + "%&!reset: Reset the command prefix to ! . (Owner Only)"
     );
   }
 })
-bot.login('nope');
+bot.login('NDIxODM4OTYyMjM2MDYzNzQ1.DYYBQA.TFgYmHBwwpBDaQeRt8CFfbB5Bow');
